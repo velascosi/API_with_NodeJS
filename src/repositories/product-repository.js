fe488d614592a314ede6d2f6afae728f09a1ac7d
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-05-26 19:32:02 -0300

    updates on archives product-controller and product-repository

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,17 +3,51 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.get = () => {
-    Product
-        .find({
-            active: true
-        }, 'title price slug');
+exports.get = async () => {
+    const res = await Product.find({
+        active: true
+    }, 'title price slug');
+    return res;
 }
 
-exports.getSlug = (slug) => {
-    return Product
+exports.getBySlug = async(slug) => {
+   const res = await Product
         .findOne({
             slug: slug,
             active: true
-        }, 'title description price slug tags')
+        }, 'title description price slug tags');
+    return res;
+}
+
+exports.getById = async(id) => {
+    const res = await Product
+        .findById(Id);
+    return res;
+}
+
+exports.getByTag = async(tag) => {
+    const res = Product
+        .findOne({
+            slug: slug,
+            active: true
+        }, 'title description price slug tags');
+}
+exports.create = async(data) => {
+    var product = new Product(data);
+    return product.save(); 
+}
+exports.update = async(id, data) => {
+    await Product
+        .findByIdAndUpdate(id, {
+            $set: {
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                slug: data. slug
+            }
+        })
+}
+exports.delete = async(id) => {
+    await Product
+        .findOneAndRemove(id);
 }
